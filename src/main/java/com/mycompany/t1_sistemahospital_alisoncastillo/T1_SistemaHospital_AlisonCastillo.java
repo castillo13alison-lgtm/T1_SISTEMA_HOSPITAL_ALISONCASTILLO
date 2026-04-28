bdbce86c3b0e31bdc1c0df61cfff273bd148dbd3
Alison Castillo <castillo.13.alison@gmail.com>
2026-04-27 19:10:04 -0500

    Primer commit - incio del proyecto

---
+++ src/main/java/com/mycompany/t1_sistemahospital_alisoncastillo/T1_SistemaHospital_AlisonCastillo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t1_sistemahospital_alisoncastillo;
+
+/**
+ *
+ * @author HP
+ */
+public class T1_SistemaHospital_AlisonCastillo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
