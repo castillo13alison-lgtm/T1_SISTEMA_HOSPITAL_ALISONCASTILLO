82082057d9e86e71bda2e83104e818a50948aa57
Alison Castillo <castillo.13.alison@gmail.com>
2026-04-27 19:30:04 -0500

    Primer commit - incio del proyecto Segundo commit - agregue la clase paciente Tercer commit - agregue la clase ControladorPacientes

---
+++ src/main/java/com/mycompany/t1_sistemahospital_alisoncastillo/ControladorPaciente.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.t1_sistemahospital_alisoncastillo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HP
+ */
+public class ControladorPaciente {
+     private ArrayList<Paciente> listaPacientes = new ArrayList<>();
+
+    public void agregarPaciente(Paciente p) {
+
+        // Validar duplicados por documento
+        for (Paciente paciente : listaPacientes) {
+            if (paciente.getNumeroDocumento().equals(p.getNumeroDocumento())) {
+                System.out.println("⚠ Paciente ya registrado.");
+                return;
+            }
+        }
+
+        listaPacientes.add(p);
+        System.out.println("✅ Paciente agregado correctamente.");
+    }
+
+    public void listarPacientes() {
+
+        if (listaPacientes.isEmpty()) {
+            System.out.println("No hay pacientes registrados.");
+        } else {
+            for (Paciente p : listaPacientes) {
+                System.out.println(p.mostrarDatos());
+                System.out.println(p.mostrarAlertaMedica());
+                System.out.println("------------------------");
+            }
+        }
+    }
+}
